1d5660b3b65ed4f949c91d38d14175400b042006
Albin <almalbin98@gmail.com>
2021-06-12 09:26:28 +0200

    Added project for dolphin context menu management

---
+++ Source/CryptoDolphinLauncher/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace CryptoDolphinLauncher
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            if (System.Diagnostics.Process.GetProcessesByName(System.IO.Path.GetFileNameWithoutExtension(System.Reflection.Assembly.GetEntryAssembly().Location)).Count() > 1) System.Diagnostics.Process.GetCurrentProcess().Kill();
+            while (true)
+            {
+                var exists = System.Diagnostics.Process.GetProcessesByName(System.IO.Path.GetFileNameWithoutExtension(System.Reflection.Assembly.GetEntryAssembly().Location)).Count() > 1;
+                if (exists)
+                    Console.WriteLine("I am running already!! :(");
+            }
+        }
+    }
+}
